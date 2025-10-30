89b4788df8127459c6072eda5cb8fdf3486b1375
Koushik1311 <programmer.koushik@gmail.com>
2025-10-30 11:48:07 +0530

    Tab bar style
    Changed the icons and when focused it will show a different icon
    added height, padding, background

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,13 +1,70 @@
-import { Tabs } from 'expo-router'
-import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { Tabs } from "expo-router";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="home" options={{ title: 'Home', tabBarIcon: ({ color }) => <MaterialIcons name="dashboard" color={color} /> }} />
-      <Tabs.Screen name="deck" options={{ title: 'Decks', tabBarIcon: ({ color }) => <MaterialCommunityIcons name="cards" color={color} /> }} />
-      <Tabs.Screen name="quiz" options={{ title: 'Quiz', tabBarIcon: ({ color }) => <MaterialIcons name='quiz' color={color} /> }} />
-      <Tabs.Screen name="settings" options={{ title: 'Settings', tabBarIcon: ({ color }) => <MaterialIcons name='settings' color={color} /> }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#F4F1EB",
+          paddingHorizontal: 10,
+          height: 80,
+          paddingBottom: 30,
+          paddingTop: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? "view-dashboard" : "view-dashboard-outline"}
+              size={24}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="deck"
+        options={{
+          title: "Decks",
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? "cards" : "cards-outline"}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="quiz"
+        options={{
+          title: "Quiz",
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? "clipboard" : "clipboard-outline"}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? "cog" : "cog-outline"}
+              size={24}
+            />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
