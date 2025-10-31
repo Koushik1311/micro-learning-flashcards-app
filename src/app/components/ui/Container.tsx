43cb67b278163ea11c304ed2cf6fe8514794410e
Koushik1311 <programmer.koushik@gmail.com>
2025-10-31 15:15:11 +0530

    Cleanup
    Removed the unused comment line

--- src/app/components/ui/Container.tsx
+++ src/app/components/ui/Container.tsx
@@ -14,7 +14,6 @@ export default function Container({
 }: ContainerProps) {
   return (
     <View className={cn("flex-1 px-4 py-2 bg-[#F4F1EB]", className)} {...props}>
-      {/* <View className={cn("flex-1 px-4 py-2", className)} {...props}> */}
       {children}
     </View>
   );
