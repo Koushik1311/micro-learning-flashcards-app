305fa4df8bf61e3ddc30d156d0595634658a9d18
Koushik1311 <programmer.koushik@gmail.com>
2025-10-31 22:49:57 +0530

    Added Study streak
    Added study streak component

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native'
+import { getAllDecks } from "@/lib/db";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import Container from "../components/ui/Container";
+import StudyStreak from "../components/ui/StudyStreak";
+import { View } from "react-native";
 
 export default function HomeScreen() {
+  const [deckCount, setDeckCount] = useState(0);
+  const router = useRouter();
+  const [completed, setCompleted] = useState<number[]>([]);
+
+  const toggleDay = (index: number) => {
+    setCompleted((prev) =>
+      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      const decks = await getAllDecks();
+      setDeckCount(decks.length);
+    })();
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+    <Container>
+      <View className="pt-8">
+        <StudyStreak />
+      </View>
+    </Container>
+  );
 }
