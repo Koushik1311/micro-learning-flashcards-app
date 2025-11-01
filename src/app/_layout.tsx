a293f45d0abc301377cce100787e5e2405c26be9
Koushik1311 <programmer.koushik@gmail.com>
2025-11-01 22:12:59 +0530

    Added react query
    added react query for getting real time data from local

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,22 +2,24 @@ import "react-native-reanimated";
 import { useEffect } from "react";
 import "../global.css";
 import { Stack } from "expo-router";
-import { getAllDecks, initDB } from "@/lib/db";
+import { initDB } from "@/lib/db";
 import { seedDummyData } from "@/lib/seed";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/query/client";
 
 export default function Layout() {
   useEffect(() => {
     (async () => {
       await initDB(); // ensure tables exist
       await seedDummyData(); // only then seed data
-      const decks = await getAllDecks();
-      console.log("✅ Decks loaded:", decks);
     })();
   }, []);
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
