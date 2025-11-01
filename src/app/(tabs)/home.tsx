79e093f3a995e2298dde1238c529df4129a29690
Koushik1311 <programmer.koushik@gmail.com>
2025-11-01 22:17:50 +0530

    Refactor
    Added ActiveDecks ans StudyStreak component and round them with custom
    Container component

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,33 +1,18 @@
-import { getAllDecks } from "@/lib/db";
-import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
 import Container from "../components/ui/Container";
 import StudyStreak from "../components/ui/StudyStreak";
-import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ActiveDecks from "../components/ui/ActiveDecks";
 
 export default function HomeScreen() {
-  const [deckCount, setDeckCount] = useState(0);
-  const router = useRouter();
-  const [completed, setCompleted] = useState<number[]>([]);
-
-  const toggleDay = (index: number) => {
-    setCompleted((prev) =>
-      prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index],
-    );
-  };
-
-  useEffect(() => {
-    (async () => {
-      const decks = await getAllDecks();
-      setDeckCount(decks.length);
-    })();
-  }, []);
-
   return (
-    <Container>
-      <View className="pt-8">
+    <SafeAreaView className="flex-1 bg-bg">
+      <Container className="pt-3">
         <StudyStreak />
-      </View>
-    </Container>
+      </Container>
+
+      <Container className="pt-12">
+        <ActiveDecks />
+      </Container>
+    </SafeAreaView>
   );
 }
