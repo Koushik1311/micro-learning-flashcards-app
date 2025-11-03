1fe18b511ada42b0760ccbc59dca68cb2586b6eb
Koushik1311 <programmer.koushik@gmail.com>
2025-11-03 19:08:03 +0530

    Refactor code for shadow
    Overide the px to 0
    added padding on ActiveDesks accourdingly and added shadow

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -10,7 +10,7 @@ export default function HomeScreen() {
         <StudyStreak />
       </Container>
 
-      <Container className="pt-12">
+      <Container className="pt-12 px-0">
         <ActiveDecks />
       </Container>
     </SafeAreaView>
--- src/app/components/ui/ActiveDecks.tsx
+++ src/app/components/ui/ActiveDecks.tsx
@@ -7,22 +7,31 @@ export default function ActiveDecks() {
 
   return (
     <View>
-      <View className="mb-3">
+      <View className="mb-3 px-4">
         <Text className="text-xl font-bold text-gray-800">Active Decks</Text>
       </View>
 
       <FlatList
         data={decks}
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+          paddingBottom: 24,
+        }}
         renderItem={({ item }) => (
-          <View className="flex-1 border border-gray-300 rounded-md mb-1 bg-surface">
-            <Link href={`/decks/${item.id}`}>
-              <View className="px-5 py-3.5">
-                <Text className="font-semibold">{item.title}</Text>
-                <Text className="text-sm text-gray-400 font-medium">
-                  {item.card_count} cards in this deck
-                </Text>
-              </View>
-            </Link>
+          <View className="overflow-visible">
+            <View
+              className="flex-1 border border-gray-300 rounded-md mb-1.5 bg-surface"
+              style={{ elevation: 6 }}
+            >
+              <Link href={`/decks/${item.id}`}>
+                <View className="px-5 py-3.5">
+                  <Text className="font-semibold">{item.title}</Text>
+                  <Text className="text-sm text-gray-400 font-medium">
+                    {item.card_count} cards in this deck
+                  </Text>
+                </View>
+              </Link>
+            </View>
           </View>
         )}
         keyExtractor={(item) => item.id}
