da29b79733649872652cbaef35842f6e5f2ac459
Koushik1311 <programmer.koushik@gmail.com>
2025-11-04 22:59:46 +0530

    useDecksWithProgress
    it let's getting decks with it's corrosponding progress

--- src/query/decks.ts
+++ src/query/decks.ts
@@ -1,9 +1,16 @@
-import { getAllDecks } from "@/db/deck.queries";
+import { getAllDecks, getDeckWithProgress } from "@/db/deck.queries";
 import { useQuery } from "@tanstack/react-query";
 
 export function useDecks() {
   return useQuery({
     queryKey: ["decks"],
     queryFn: getAllDecks,
+  });
+}
+
+export function useDecksWithProgress() {
+  return useQuery({
+    queryKey: ["decks-with-progress"],
+    queryFn: getDeckWithProgress,
   });
 }
