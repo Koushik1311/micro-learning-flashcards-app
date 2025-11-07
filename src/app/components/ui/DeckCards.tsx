b8e0375b6904d596ba0fef95e749b87448a201b0
Koushik1311 <programmer.koushik@gmail.com>
2025-11-07 20:54:26 +0530

    Added header and line
    added header and line

--- src/app/components/ui/DeckCards.tsx
+++ src/app/components/ui/DeckCards.tsx
@@ -16,6 +16,7 @@ export default function DeckCards() {
       keyExtractor={(item) => item.id}
       renderItem={({ item, index }) => <DeckRow item={item} index={index} />}
       showsVerticalScrollIndicator={false}
+      ListHeaderComponent={<ListHeader />}
     />
   );
 }
@@ -29,18 +30,34 @@ function DeckRow({ item, index }) {
       from={{ opacity: 0, translateY: 10 }}
       animate={{ opacity: 1, translateY: 0 }}
       transition={{ delay: index * 60, duration: 250 }}
-      className="rounded-xl p-4 mb-3"
     >
-      <View className="flex-row justify-between items-center">
+      <View className="flex-row justify-between items-center mx-4">
         <View className="flex-1">
-          <Text className="text-base font-semibold">{item.title}</Text>
-          <Text className="text-xs text-gray-400 mt-1">
+          <Text className="text-lg font-medium text-stone-800">
+            {item.title}
+          </Text>
+          <Text className="text-sm text-[#373739]">
             {item.progress.learned} / {item.progress.total} cards
           </Text>
         </View>
 
-        <Text className="text-sm font-medium">{pct.toFixed(1)}%</Text>
+        <Text className="text-stone-800">{pct.toFixed(1)}%</Text>
       </View>
+
+      <View className="border-b border-stone-300 w-screen my-5" />
     </MotiView>
+  );
+}
+
+function ListHeader() {
+  return (
+    <View className="flex-row justify-between px-4 mb-5 mt-2">
+      <Text className="text-[12px] font-semibold text-stone-500 tracking-wider">
+        DECK TITLE
+      </Text>
+      <Text className="text-[12px] font-semibold text-stone-500 tracking-wider">
+        COMPLETE
+      </Text>
+    </View>
   );
 }
