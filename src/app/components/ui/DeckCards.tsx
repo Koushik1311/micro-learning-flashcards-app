a2fce3f1379cba457ec0f3e492a8c81a05e44b39
Koushik1311 <programmer.koushik@gmail.com>
2025-11-08 14:28:39 +0530

    Removed logs and linked the list items
    removed console log as it is not needed
    added touchable opacity and added route to it to go to the corrosponding
    flashcard

--- src/app/components/ui/DeckCards.tsx
+++ src/app/components/ui/DeckCards.tsx
@@ -1,10 +1,10 @@
 import { useDecksWithProgress } from "@/query/decks";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { MotiView } from "moti";
+import { router } from "expo-router";
 
 export default function DeckCards() {
   const { data: decks, isLoading } = useDecksWithProgress();
-  console.log("Decks with progress: ", decks);
 
   if (isLoading) return <Text className="text-center mt-6">Loading...</Text>;
   if (!decks || decks.length === 0)
@@ -23,29 +23,33 @@ export default function DeckCards() {
 
 function DeckRow({ item, index }) {
   const pct = item.progress.percent ?? 0;
-  console.log("Rendering deck row:", item.title);
 
   return (
-    <MotiView
-      from={{ opacity: 0, translateY: 10 }}
-      animate={{ opacity: 1, translateY: 0 }}
-      transition={{ delay: index * 60, duration: 250 }}
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={() => router.push(`/flashcard/${item.id}`)}
     >
-      <View className="flex-row justify-between items-center mx-4">
-        <View className="flex-1">
-          <Text className="text-lg font-medium text-stone-800">
-            {item.title}
-          </Text>
-          <Text className="text-sm text-[#373739]">
-            {item.progress.learned} / {item.progress.total}
-          </Text>
+      <MotiView
+        from={{ opacity: 0, translateY: 10 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ delay: index * 60, duration: 250 }}
+      >
+        <View className="flex-row justify-between items-center mx-4">
+          <View className="flex-1">
+            <Text className="text-lg font-medium text-stone-800">
+              {item.title}
+            </Text>
+            <Text className="text-sm text-[#373739]">
+              {item.progress.learned} / {item.progress.total}
+            </Text>
+          </View>
+
+          <Text className="text-stone-800">{pct.toFixed(1)}%</Text>
         </View>
 
-        <Text className="text-stone-800">{pct.toFixed(1)}%</Text>
-      </View>
-
-      <View className="border-b border-stone-300 w-screen my-5" />
-    </MotiView>
+        <View className="border-b border-stone-300 w-screen my-5" />
+      </MotiView>
+    </TouchableOpacity>
   );
 }
 
